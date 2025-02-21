0e5966714527a89551d6670a008ac0c7d2e8d0f9
mohamed <88539841+koualil@users.noreply.github.com>
2025-02-21 22:46:47 +0000

    first completed

--- Answers/answer_1.py
+++ Answers/answer_1.py
@@ -2,15 +2,42 @@ import pandas as pd
 import os
 
 def CheckHeader(path):
-    pass
+    header = ["Date","Low","Open","Volume","High","Close","Adjusted Close"]
+    try:
+        df = pd.read_csv(path)
+    except:
+        print(f"cannot open file {path}")
+        exit(1)
 
-def TabDelim(paths):
-    pass
+    if list(df.columns) != header:
+        df = df[header]
+    return df
+    
+def Combine_files(files):
+    dfs =[]
+    for file in files:
+        dfs.append(CheckHeader(file))
+    df_combined = pd.concat(dfs)
+    df_combined.to_csv('fileCombined.csv', index=False, sep='\t')
+    print("Files combined successfully.")
+
+
 
 
 
 
 
 if __name__ == "__main__":
+    
+    path = "../CSVs"
+    try:
+        folder = os.listdir(path)
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        exit(1)
+    files_input = [path+"/"+file for file in folder if file.split('.')[1] == 'csv']
+    Combine_files(files_input)
+
+
 
 
